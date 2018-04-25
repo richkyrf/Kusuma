995c24ec295477d6fdd005db78b891545e65d0a7
richkyrf <35827460+richkyrf@users.noreply.github.com>
2018-04-25 23:28:36 +0700

    checkpoint commit

--- build/classes/File/DBReadThreadAndGUITest.rs
+++
@@ -1,3 +0,0 @@
-File.DBReaderThread
-File.DBReadThreadAndGUITest
-File.GUIThread
--- src/Proses/Perawatan.java
+++ src/Proses/Perawatan.java
@@ -750,6 +750,18 @@ public class Perawatan extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
